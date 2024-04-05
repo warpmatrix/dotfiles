3729c2631a10d16753fd79a5a8a9d6f2397e0b51
warpmatrix <warpmatrixlyk@gmail.com>
2024-04-06 00:57:34 +0800

    feat: autostart tmux within fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,13 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
+    if not set -q TMUX
+        if tmux has-session -t home
+            exec tmux attach-session -t home
+        else
+            tmux new-session -s home
+        end
+    end
     if command -v pyenv &> /dev/null
         pyenv init - | source
-     end
+    end
 end
